98eab9dde58a39b710efa05f674a05748281ab1e
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-26 12:52:49 -0500

    initial and final commit

--- GestureRecognizerMiniApp/ViewController.swift
+++ GestureRecognizerMiniApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var eyesOutlet: UIImageView!
+    @IBOutlet weak var mouthOutlet: UIImageView!
+    @IBOutlet weak var handOutlet: UIImageView!
+    @IBOutlet weak var lightsaberOutlet: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func eyesTapRecognizerOutlet(_ sender: UITapGestureRecognizer) {
+        
+        var tappedLoc = sender.location(in: view)
+        eyesOutlet.center = tappedLoc
+    }
+    
+    
+    @IBAction func mouthPanOutlet(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc = sender.location(in: view)
+        mouthOutlet.center = pannedLoc
+    }
+    
+    @IBAction func handPanOutlet(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc = sender.location(in: view)
+        handOutlet.center = pannedLoc
+    }
+    
+    @IBAction func lightsaberPanOutlet(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc = sender.location(in: view)
+        lightsaberOutlet.center = pannedLoc
+    }
+    
+    
+    
 }
 
