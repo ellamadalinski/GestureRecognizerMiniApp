1c4ae0598cfd13a377bc01a399612a3fc9ce4050
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-29 08:10:31 -0500

    added camera and photo - done

--- GestureRecognizerMiniApp/ViewController.swift
+++ GestureRecognizerMiniApp/ViewController.swift
@@ -7,16 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UINavigationControllerDelegate , UIImagePickerControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
     
     @IBOutlet weak var eyesOutlet: UIImageView!
     @IBOutlet weak var mouthOutlet: UIImageView!
     @IBOutlet weak var handOutlet: UIImageView!
     @IBOutlet weak var lightsaberOutlet: UIImageView!
+    @IBOutlet weak var imageViewOutlet: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     
@@ -42,7 +45,26 @@ class ViewController: UIViewController {
         lightsaberOutlet.center = pannedLoc
     }
     
+    @IBAction func cameraButtonAction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+        else{
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+    }
     
+    @IBAction func photosButtonAction(_ sender: UIButton) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true) {
+            self.imageViewOutlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        }
+    }
     
 }
 
